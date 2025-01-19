95e6d4724703af80cb236fd22706ca2c0780655f
Hasan-al-Mashruf <mashruf125@gmail.com>
2025-01-19 11:28:16 +0600

    fat: folder structure integrated

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+
+// cors congiguration...
+const corsConfig = {
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST"],
+};
+// assets
+const port = process.env.PORT;
+const dbUrl = process.env.MONGO_URI;
+const app = express();
+
+// Middleware
+app.use(cors({ ...corsConfig }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to my server" });
+});
+
+// connect to db
+mongoose
+  .connect(dbUrl)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error("Could not connect to MongoDB", error));
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
